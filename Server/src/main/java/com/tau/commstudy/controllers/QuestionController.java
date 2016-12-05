9607688a31f891c1f48d5012a6aeae3a4c90e3d2
omri <omri@tokenads.com>
2016-12-05 22:35:15 +0200

    added @CrossOrigin to getAll function - so we could make ajax calls from client side ti server

--- Server/src/main/java/com/tau/commstudy/controllers/QuestionController.java
+++ Server/src/main/java/com/tau/commstudy/controllers/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -45,6 +46,7 @@ public class QuestionController {
 	return dao.findOne(id);
     }
     
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, value = "/all")
     public Iterable<Question> getAll()
     {

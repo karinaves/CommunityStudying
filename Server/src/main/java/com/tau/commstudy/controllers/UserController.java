383a72422ba7c8c8225e34dc0fa72326b5564492
omri <omri@tokenads.com>
2016-12-09 18:11:43 +0200

    added exception to user controller

--- Server/src/main/java/com/tau/commstudy/controllers/UserController.java
+++ Server/src/main/java/com/tau/commstudy/controllers/UserController.java
@@ -1,26 +1,37 @@
 package com.tau.commstudy.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import java.lang.RuntimeException;
+
 import com.tau.commstudy.entities.User;
+import com.tau.commstudy.exceptions.UnauthorizesException;
 import com.tau.commstudy.services.UsersService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    
+
     @Autowired
     private UsersService userService;
-    
+
     @RequestMapping(method = RequestMethod.GET, value = "/getOrCreateUser")
-    public User getOrCreateUser(String idTokenString) throws Exception{
-	User user =userService.getOrCreateUser(idTokenString);
-	if (user == null){
-	    throw new Exception();
+    public User getOrCreateUser(String idTokenString) throws Exception {
+	User user = userService.getOrCreateUser(idTokenString);
+	if (user == null) {
+	    throw new UnauthorizesException();
 	}
-	return user;	
+	return user;
     }
+
+    @ExceptionHandler(UnauthorizesException.class)
+    @ResponseStatus(code=HttpStatus.UNAUTHORIZED)
+    public String handleException(UnauthorizesException e) {
+	return e.getMessage();
+    }
+
 }
---
+++ Server/src/main/java/com/tau/commstudy/exceptions/UnauthorizesException.java
@@ -0,0 +1,10 @@
+package com.tau.commstudy.exceptions;
+
+public class UnauthorizesException extends RuntimeException {
+	private static final long serialVersionUID = 7023907624084830848L;
+	
+	public UnauthorizesException()
+	{
+	    super("Invalid user token");
+	}
+}

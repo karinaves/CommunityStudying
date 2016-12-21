6a275a873e571e34ed1b6df6b2d43342a8613428
omri <omri@tokenads.com>
2016-12-21 22:44:25 +0200

    added CrossOrigin - temporary

--- Server/src/main/java/com/tau/commstudy/controllers/FacultyController.java
+++ Server/src/main/java/com/tau/commstudy/controllers/FacultyController.java
@@ -1,6 +1,7 @@
 package com.tau.commstudy.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.tau.commstudy.services.FacultyService;
 
 @RestController
 @RequestMapping("/faculty")
+@CrossOrigin
 public class FacultyController {
 
     @Autowired

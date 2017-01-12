df1813b275b753b7aee87aa6861434c5230ac06e
ohirshman <hirshmanor@gmail.com>
2017-01-12 10:46:18 +0200

    files controller added

--- Server/src/main/java/com/tau/commstudy/controllers/FilesController.java
+++ Server/src/main/java/com/tau/commstudy/controllers/FilesController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,19 +12,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.tau.commstudy.services.FileService;
 
 @RestController
 @RequestMapping("/files")
 @CrossOrigin
 public class FilesController {
+
+    @Autowired
+    private FileService fileService;
+
     public static final String uploadingdir = System.getProperty("C:\\files");
 
     @RequestMapping(value = "/upload2", method = RequestMethod.POST)
@@ -40,29 +38,7 @@ public class FilesController {
     public boolean uploadContentFile(@RequestParam("uploadingFiles") MultipartFile[] uploadingFiles)
 	    throws IOException {
 
-	String accessKeyID = "AKIAJBJANY4WXG3C3MTA";
-	String secretAccessKey = "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";
-
-	AWSCredentials credentials = new BasicAWSCredentials(accessKeyID, secretAccessKey);
-	String keyName = "***";
-	String bucketName = "study-buddy-files";
-	try {
-	    AmazonS3 s3client = new AmazonS3Client(credentials);
-	    for (MultipartFile uploadedFile : uploadingFiles) {
-		File file = convert(uploadedFile);
-		keyName = uploadedFile.getOriginalFilename();
-		s3client.putObject(new PutObjectRequest(bucketName, keyName, file).withCannedAcl(CannedAccessControlList.PublicRead));
-		System.out.println(file.exists());
-	    }
-
-	} catch (AmazonServiceException ase) {
-	    return false;
-	} catch (AmazonClientException ace) {
-
-	    return false;
-	}
-	System.out.println(keyName);
-	return true;
+	return fileService.uploadFiles(uploadingFiles);
     }
 
     public File convert(MultipartFile file) throws IOException {

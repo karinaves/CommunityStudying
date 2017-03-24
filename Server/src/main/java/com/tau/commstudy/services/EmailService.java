4f4306016b67c974d2574e13c34aa1f12c58724d
ohirshman <hirshmanor@gmail.com>
2017-03-24 13:22:43 +0300

    html email service

--- Server/src/main/java/com/tau/commstudy/services/EmailService.java
+++ Server/src/main/java/com/tau/commstudy/services/EmailService.java
@@ -6,12 +6,18 @@ import java.io.IOException;
 import java.sql.Date;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -27,16 +33,19 @@ public class EmailService {
     @Value("${clientDomain}")
     private String clientDomain;
 
+    @Value("${url}")
+    private String url;
+
     private String emailUsername = null;
     private String emailPassword = null;
 
-    public Boolean emailComment(String email, String title, Long postId, String name) throws AddressException,
-	    MessagingException {
+    public Boolean emailComment(String email, String title, Long postId, String name)
+	    throws AddressException, MessagingException {
 	Properties props = System.getProperties();
 	String[] keys = getCredentials();
 	String mail = keys[0];
 	String password = keys[1];
-	String postPath = clientDomain + "/#/questions/view/" + postId;
+	String postPath = url + "/#/questions/view/" + postId;
 	props.put("mail.smtps.host", "smtp.gmail.com");
 	props.put("mail.smtps.auth", "true");
 	Session session = Session.getInstance(props, null);
@@ -44,10 +53,27 @@ public class EmailService {
 	msg.setFrom(new InternetAddress(mail));
 	;
 	msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email, false));
+
 	msg.setSubject("התקבלה תגובה חדשה לשאלתך");
-	msg.setText("שלום " + name + "\n" + "התקבלה תגובה חדשה לשאלתך: " + title + " .\nתוכל/י לצפות בתגובה בלינק:\n"
-		+ postPath);
-	msg.setHeader("X-Mailer", "Tov Are's program");
+	MimeMultipart multipart = new MimeMultipart("related");
+
+	// first part (the html)
+	BodyPart messageBodyPart = new MimeBodyPart();
+	String htmlText = "<head>" + "<style type=\"text/css\">" + "  .blue { color: #00f; }" + "</style>" + "</head>"
+		+ "<body dir='rtl'>" + "<h3 class=\"blue\">" + "שלום " + name + "," + "</h3>" + "<p>" + ""
+		+ "<strong>התקבלה תגובה חדשה לשאלתך: </strong>" + title + "." + "</p>" + "<p>" + "לצפייה " + "<a href="
+		+ postPath + " >לחץ כאן</a></p>" + "<img src=\'cid:image\'>" + "</body>";
+	messageBodyPart.setContent(htmlText, "text/html; charset=utf-8");
+	multipart.addBodyPart(messageBodyPart);
+
+	// second part (the image)
+	messageBodyPart = new MimeBodyPart();
+	DataSource fds = new FileDataSource("C:\\wamp\\www\\CommunityStudying-Client\\public_html\\img\\logo.png");
+	messageBodyPart.setDataHandler(new DataHandler(fds));
+	messageBodyPart.setHeader("Content-ID", "<image>");
+	multipart.addBodyPart(messageBodyPart);
+	msg.setContent(multipart);
+
 	msg.setSentDate(new java.util.Date());
 	SMTPTransport t = (SMTPTransport) session.getTransport("smtps");
 	t.connect("smtp.gmail.com", mail, password);
@@ -67,12 +93,30 @@ public class EmailService {
 	Session session = Session.getInstance(props, null);
 	Message msg = new MimeMessage(session);
 	msg.setFrom(new InternetAddress(mail));
-	;
 	msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email, false));
-	msg.setSubject("Welcome to Study buddy TAU!");
-	msg.setText("שלום " + name + "\n\r"
-		+ ".ברוכ/ה הבא/ה, זה המקום בו תוכל/י לשאול שאלות על מבחנים ולקבל תשובות במהירות ובקלות.");
-	msg.setHeader("X-Mailer", "Tov Are's program");
+	msg.setSubject("ברוכים הבאים לסטדי-באדי!");
+
+	MimeMultipart multipart = new MimeMultipart("related");
+
+	// first part (the html)
+	BodyPart messageBodyPart = new MimeBodyPart();
+	String htmlText = "<head>" + "<style type=\"text/css\">" + "  .blue { color: #00f; }" + "</style>" + "</head>"
+		+ "<body dir='rtl'>" + "<h3 class=\"blue\">" + "שלום " + name + "," + "</h3>" + "<p>" + "ברוך הבא, "
+		+ "<strong>סטדי-באדי</strong>" + " זה המקום בו תוכל לשאול שאלות על מבחנים ולקבל תשובות בקלות ובמהירות."
+		+ "</p>" + "<p>" + "לכניסה לאתר " + "<a href='http://www.studybuddytau.ml/#/'>לחץ כאן</a></p>"
+
+		+ "<img src=\'cid:image\'>" + "</body>";
+	messageBodyPart.setContent(htmlText, "text/html; charset=utf-8");
+	multipart.addBodyPart(messageBodyPart);
+
+	// second part (the image)
+	messageBodyPart = new MimeBodyPart();
+	DataSource fds = new FileDataSource("C:\\wamp\\www\\CommunityStudying-Client\\public_html\\img\\logo.png");
+	messageBodyPart.setDataHandler(new DataHandler(fds));
+	messageBodyPart.setHeader("Content-ID", "<image>");
+	multipart.addBodyPart(messageBodyPart);
+	msg.setContent(multipart);
+
 	msg.setSentDate(new Date(0));
 	SMTPTransport t = (SMTPTransport) session.getTransport("smtps");
 	t.connect("smtp.gmail.com", mail, password);
